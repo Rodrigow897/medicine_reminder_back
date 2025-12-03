44d7c9a7a421d055262503ffffd1eae8953103a1
Rodrigo <rwesley2000@gmail.com>
2025-12-03 10:24:04 -0300

    feat: added database

---
+++ src/dataBase/db.js
@@ -0,0 +1,15 @@
+const {Pool} = require('pg')
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'reminder_db',
+    password: 'wcc@2023',
+    port: 5432
+});
+
+pool.connect()
+.then(() => console.log('Connected to the database'))
+.catch(err => console.error('Connection failed', err));
+
+module.exports = pool
